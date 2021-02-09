0db20f64f37e20888d6d1a443bce0a376441fb16
tone53 <tone_53@hotmail.com>
2021-02-09 18:32:02 +0700

    Update trmline.sh
    update jak kung version

--- trmline.sh
+++ trmline.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 CONFIG_FILE="/root/config.txt"
 source ${CONFIG_FILE}
-#echo "Rig name: ${rigName}"
-linetoken=$'xxxxxxxline token xxxxxxxxxxxxxx'
-minerapi=$'localhost 3333'
-echo '{"method": "miner_getstat1", "jsonrpc": "2.0", "id": 5 }' | nc $minerapi > api.json
-sleep 2
-totalhash=$(jq '.result[2]' api.json)
-hash=$(jq '.result[3]' api.json)
-temp=$(jq '.result[6]' api.json)
-uptimemin=$(uptime | awk -F ',' ' {print $1} ' | awk ' {print $3} ' | awk -F ':' ' {hrs=$1; min=$2; print min} ')
-uptimehrs=$(uptime | awk -F ',' ' {print $1} ' | awk ' {print $3} ' | awk -F ':' ' {hrs=$1; min=$2; print hrs} ')
-name=$(hostname)
+linetoken=$'xxxxLine Tokenxxxxxxxx'
+TOTAL_HASH=`cat /var/tmp/stats_hash 2>/dev/null || echo '{}'`
+UPTIME=`cat /var/tmp/stats_sys_uptime 2>/dev/null || echo '{}'`
+GPU_HASH=`cat /var/tmp/stats_gpu_hash_jq 2>/dev/null || echo '{}'`
+GPU_COUNT=`cat /var/tmp/stats_gpu_count 2>/dev/null || echo 0`
+GPU_TEMP=`cat /var/tmp/stats_gpu_temp_jq 2>/dev/null || echo '{}'`
+GPU_FAN=`cat /var/tmp/stats_gpu_fan_jq 2>/dev/null || echo '{}'`
+THASH=`echo ${TOTAL_HASH} | awk '{ printf "%.2f\n", $1/1000000 }'`
+for ((i=0; i<${GPU_COUNT}; i++)); do
+  IGPU_HASH[${i}]=`echo ${GPU_HASH} | jq -r '.["'${i}'"]' | awk '{ printf "%.2f\n", $1/1000000 }'`
+  IGPU_TEMP[${i}]=`echo ${GPU_TEMP} | jq -r '.["'${i}'"] // "n/a"'`
+  IGPU_FAN[${i}]=`echo ${GPU_FAN} | jq -r '.["'${i}'"] // "n/a"'`
+done
 NL=$'\n'
-msg="Worker:${rigName}: $uptimehrs:$uptimemin$NL+Total: $totalhash$NL+Hash: $hash$NL+Temp: $temp"
+msg="${rigName}+/+${GPU_COUNT} GPU/${UPTIME}$NL+Hash: ${THASH} Mhs$NL+>+${IGPU_HASH[0]},${IGPU_HASH[1]},${IGPU_HASH[2]},${IGPU_HASH[3]},${IGPU_HASH[4]},${IGPU_HASH[4]}$NL+Temp: ${IGPU_TEMP[0]},${IGPU_TEMP[1]},${IGPU_TEMP[2]},${IGPU_TEMP[3]},${IGPU_TEMP[4]},${IGPU_TEMP[5]}"
 curl -X POST -H "Authorization: Bearer $linetoken" -H "Content-Type: application/x-www-form-urlencoded" -d "message=$msg" https://notify-api.line.me/api/notify
 sleep 2
 exit 0
